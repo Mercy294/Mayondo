260688fad531f57f48ce5bdd415d71a79c8f5011
Ayuru Mercy <mercyniblett11@gmail.com>
2026-06-09 13:33:09 +0300

    Update ALLOWED_HOSTS for deployment

--- mwfproject/mwfproject/settings.py
+++ mwfproject/mwfproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8xb_35ilq8=1c#hu0cbhq_t=yzpou-v)w^sxte%bk_0%i1*4pi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mayondo-g443.onrender.com', 'localhost', '127.0.0.1', '*']
 
 
 # Application definition
